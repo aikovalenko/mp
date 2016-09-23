4a27a9accfea9925e9d6a1b5b1775e6f00a5351d
aikovalenko <kovalenko.ai@yandex.ru>
2016-09-23 03:19:28 +0200

    Basics2

---
+++ app/js/common.js
@@ -0,0 +1,41 @@
+$(window).on('load', function () {
+    var preloader = $('.block_preloader'),
+        itemN = $('.projects .block');
+    setTimeout(function(){
+        preloader.css({ "z-index": "0", "opacity": "0"});
+        itemN.css({ "filter": "blur(0)"});
+        /*
+        $.each(itemN, function(i, el) {
+            setTimeout(function() {
+                $(el).css({ "transform": "translateY(0px)", "opacity": "1"});
+            }, (i * 200));
+        });
+        */
+
+    }, 1000);
+
+    function preloadImages() {
+
+        for(var i = 0; i<arguments.length; i++) {
+            $("<img />").attr("src", arguments[i]);
+        }
+    }
+    preloadImages("images/des/project1.jpg","images/des/project2.jpg","images/des/project3.jpg","images/des/project4.jpg",
+        "images/des/project5.jpg","images/des/project6.jpg","images/des/project7.jpg","images/des/project8.jpg");
+});
+$(document).ready(function() {
+
+    $("#directions").on("click", function(){
+        event.preventDefault();
+        if ($(".menu .intro").hasClass( "open" )) {
+            $(".menu .intro").removeClass( "open" );
+            $(".menu ul").addClass( "open" );
+        }
+        else {
+            $(".menu .intro").addClass( "open" );
+            $(".menu ul").removeClass( "open" );
+        }
+        //$(".menu .intro").toggleClass( "open" );
+        //$(".menu ul").toggleClass( "open" );
+    });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,13 @@ var gulp       = require('gulp'), // Подключаем Gulp
     autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('app/sass/**/*.sass') // Берем источник
+    return gulp.src('app/scss/**/*.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-        .pipe(csscomb())
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(cssnano()) // Сжимаем
         .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
         .pipe(gulp.dest('app/css')) // Выгружаем в папку app/css
-        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+        .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -47,11 +45,12 @@ gulp.task('css-libs', ['sass'], function() {
     return gulp.src('app/css/libs.css') // Выбираем файл для минификации
         .pipe(cssnano()) // Сжимаем
         .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+        .pipe(gulp.dest('app/css')) // Выгружаем в папку app/css
+        .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
 });
 
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-    gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/scss/**/*.scss', ['sass'], browserSync.reload); // Наблюдение за sass файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
